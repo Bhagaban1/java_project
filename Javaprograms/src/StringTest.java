5282d2d15c1bea8392b3cdbaa2d9474a7e25698a
Bhagaban <Bhagaban@Bhagaban-PC>
2019-12-02 14:32:22 +0530

    first commit uploading all the files

---
+++ Javaprograms/src/StringTest.java
@@ -0,0 +1,15 @@
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="java";
+		String s2="java";
+		//String s=s+s1;
+		System.out.println(s1);
+		System.out.println(s2);
+		
+
+	}
+
+}
